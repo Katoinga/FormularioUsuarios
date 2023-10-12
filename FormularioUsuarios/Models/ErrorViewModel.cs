3217f1a48f42804e3362c1963931447bd2d7b421
Katoinga <evaldiviaci@unsa.edu.pe>
2023-10-12 16:45:57 -0500

    first commit

---
+++ FormularioUsuarios/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FormularioUsuarios.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
