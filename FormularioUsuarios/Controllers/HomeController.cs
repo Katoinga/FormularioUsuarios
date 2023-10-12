d0316613631e307d2f7cbd685192b4019cec4a03
Katoinga <evaldiviaci@unsa.edu.pe>
2023-10-12 18:44:08 -0500

    feat: create form

--- FormularioUsuarios/Controllers/HomeController.cs
+++ FormularioUsuarios/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using FormularioUsuarios.Models.ViewModels;
 
 namespace FormularioUsuarios.Controllers
 {
@@ -21,7 +22,35 @@ namespace FormularioUsuarios.Controllers
             return View(list);
         }
 
-        
+        [HttpGet] 
+        public IActionResult DetalleUsuario()
+        {
+            UsuarioVM oUsuario = new UsuarioVM();
+            return View(oUsuario);
+        }
+        [HttpPost]
+        public IActionResult GuardarUsuario(UsuarioVM oUsuarioVM)
+        {
+            if (ModelState.IsValid)
+            {
+                Usuario nuevo = new Usuario();
+                nuevo.Nombres = oUsuarioVM.Nombres;
+                nuevo.Apellidos = oUsuarioVM.Apellidos;
+                nuevo.FechaNacimiento = oUsuarioVM.FechaNacimiento;
+                nuevo.NroDocumento = oUsuarioVM.NroDocumento;
+                nuevo.Departamento = oUsuarioVM.Departamento;
+                nuevo.Provincia = oUsuarioVM.Provincia;
+                nuevo.Direccion = oUsuarioVM.Direccion;
+                nuevo.Estado = oUsuarioVM.Estado;
+                _DBContext.Usuarios.Add(nuevo);
+                _DBContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View("DetalleUsuario", oUsuarioVM);
+        }
+
+
+
 
     }
 }
---
+++ FormularioUsuarios/Models/ViewModels/UsuarioVM.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormularioUsuarios.Models.ViewModels
+{
+    public class UsuarioVM
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo Nombres es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El campo Nombres no puede tener más de 50 caracteres")]
+        public string Nombres { get; set; } = null!;
+
+        [Required(ErrorMessage = "El campo Apellidos es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El campo Apellidos no puede tener más de 50 caracteres")]
+        public string Apellidos { get; set; } = null!;
+
+        [Required(ErrorMessage = "El campo Fecha de Nacimiento es obligatorio")]
+        [Display(Name = "Fecha de Nacimiento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "El campo Numero de Documento es obligatorio")]
+        public int NroDocumento { get; set; }
+
+        [Required(ErrorMessage = "El campo Departamento es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El campo Departamento no puede tener más de 50 caracteres")]
+        public string Departamento { get; set; } = null!;
+
+        [Required(ErrorMessage = "El campo Provincia es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El campo Provincia no puede tener más de 50 caracteres")]
+        public string Provincia { get; set; } = null!;
+
+        [Required(ErrorMessage = "El campo Direccion es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El campo Direccion no puede tener más de 50 caracteres")]
+        public string Direccion { get; set; } = null!;
+        public bool? Estado { get; set; }
+    }
+}
